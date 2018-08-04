6b0be11349e60179107d732635c682716bcf8c18
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-08-04 12:54:27 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,4 @@
 #write your code here
-number = 10
 
 def countdown(number)
   while number > 0
@@ -10,12 +9,10 @@ def countdown(number)
   "Happy New Year!".upcase
 end
 
-newnumber = 10
-
-def countdown_with_sleep(newnumber)
-  while newnumber > 0
-    puts "#{newnumber} seconds(s)!\n".upcase
-    newnumber -= 1
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} seconds(s)!\n".upcase
+    number -= 1
     sleep 6
   end
 
