9ed5aa91c6246f3d2bc56728a56f4670e39c237b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-08-04 12:52:42 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,13 +10,13 @@ def countdown(number)
   "Happy New Year!".upcase
 end
 
-number = 10
+newnumber = 10
 
-def countdown_with_sleep(number)
-  while number > 0
-    puts "#{number} seconds(s)!\n".upcase
-    number -= 1
-    sleep(5)
+def countdown_with_sleep(newnumber)
+  while newnumber > 0
+    puts "#{newnumber} seconds(s)!\n".upcase
+    newnumber -= 1
+    sleep 6
   end
 
   "Happy New Year!".upcase
